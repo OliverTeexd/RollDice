d40c1bb18ee0c464409dc5336f9ee7c9ba5cc711
anreh <anurehka@gmail.com>
2019-10-16 12:23:40 +0800

    Commited

---
+++ app/src/main/java/com/example/rolldice/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.rolldice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+    private fun rollDice(){
+        Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()
+    }
+}
